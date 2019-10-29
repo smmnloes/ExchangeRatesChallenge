fd5e9acdb6813c19d5b6c3d8d03dc79b473b9336
maximilian <m.loesch@hotmail.de>
2019-10-29 15:55:42 +0100

    first commit

---
+++ src/test/java/it/mloesch/BK_Challenge/BkChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mloesch.BK_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
